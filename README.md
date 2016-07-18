c2958ece2f2be99c6de1cdf7e1770038f500ba37
Adam Buczynski <adambuczynski@gmail.com>
2016-07-18 19:38:17 +1200

    Update build process, use ESLint, Babel

--- README.md
+++ README.md
@@ -12,9 +12,7 @@ An Angular service with various conversion helpers
 
 ## Installation
 
-You can install this package using `npm` or `bower`.
-
-### npm
+You can install this package using `npm`:
 
 ```shell
 npm install meanie-angular-convert --save
@@ -24,20 +22,6 @@ Include the script `node_modules/meanie-angular-convert/release/meanie-angular-c
 
 ```html
 <script src="node_modules/meanie-angular-convert/release/meanie-angular-convert.js"></script>
-```
-
-Add `Convert.Service` as a dependency for your app.
-
-### bower
-
-```shell
-bower install meanie-angular-convert
-```
-
-Include the script `bower_components/meanie-angular-convert/release/meanie-angular-convert.js` in your build process, or add it via a `<script>` tag to your `index.html`:
-
-```html
-<script src="bower_components/meanie-angular-convert/release/meanie-angular-convert.js"></script>
 ```
 
 Add `Convert.Service` as a dependency for your app.
--- release/meanie-angular-convert.js
+++ release/meanie-angular-convert.js
@@ -1,372 +1,359 @@
 /**
- * meanie-angular-convert - v1.2.2 - 10-1-2016
+ * meanie-angular-convert - v1.2.2 - 18-6-2016
  * https://github.com/meanie/angular-convert
  *
  * Copyright (c) 2016 Adam Buczynski <me@adambuczynski.com>
  * License: MIT
  */
-(function(window, angular, undefined) {'use strict';
+'use strict';
 
-/**
- * Module definition and dependencies
- */
-angular.module('Convert.Service', [
-  'Convert.StringConverter.Service',
-  'Convert.ObjectConverter.Service',
-  'Convert.QueryStringConverter.Service'
-])
+(function (window, angular, undefined) {
+  'use strict';
 
-/**
- * Factory definition
- */
-.factory('$convert', ['$stringConverter', '$objectConverter', '$queryStringConverter', function($stringConverter, $objectConverter, $queryStringConverter) {
-  return {
-    string: $stringConverter,
-    object: $objectConverter,
-    queryString: $queryStringConverter
-  };
-}]);
+  /**
+   * Module definition and dependencies
+   */
 
+  angular.module('Convert.Service', ['Convert.StringConverter.Service', 'Convert.ObjectConverter.Service', 'Convert.QueryStringConverter.Service'])
+
+  /**
+   * Factory definition
+   */
+  .factory('$convert', ['$stringConverter', '$objectConverter', '$queryStringConverter', function ($stringConverter, $objectConverter, $queryStringConverter) {
+    return {
+      string: $stringConverter,
+      object: $objectConverter,
+      queryString: $queryStringConverter
+    };
+  }]);
 })(window, window.angular);
+'use strict';
 
-(function(window, angular, undefined) {'use strict';
-
-/**
- * Module definition and dependencies
- */
-angular.module('Convert.ObjectConverter.Service', [
-  'Convert.StringConverter.Service'
-])
-
-/**
- * Factory definition
- */
-.factory('$objectConverter', ['$stringConverter', function($stringConverter) {
+(function (window, angular, undefined) {
+  'use strict';
 
   /**
-   * Helper to convert object keys using a specified string converter
+   * Module definition and dependencies
    */
-  function convertObjectKeys(obj, converter) {
 
-    //Must have object
-    if (!angular.isObject(obj)) {
-      throw new TypeError('Not an object');
-    }
-
-    //Validate case
-    if (typeof $stringConverter[converter] === 'undefined') {
-      throw new Error('Invalid converter: ' + converter);
-    }
-
-    //Initialize object
-    var newObj = {};
-
-    //Loop keys
-    for (var key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        var newKey = $stringConverter[converter](key);
-        newObj[newKey] = angular.copy(obj[key]);
-      }
-    }
-
-    //Return object
-    return newObj;
-  }
+  angular.module('Convert.ObjectConverter.Service', ['Convert.StringConverter.Service'])
 
   /**
-   * Object converter
+   * Factory definition
    */
-  return {
+  .factory('$objectConverter', ['$stringConverter', function ($stringConverter) {
 
     /**
-     * Convert object keys to snake case
+     * Helper to convert object keys using a specified string converter
      */
-    keysToSnakeCase: function(obj) {
-      return convertObjectKeys(obj, 'toSnakeCase');
-    },
+    function convertObjectKeys(obj, converter) {
 
-    /**
-     * Convert object keys to snake case
-     */
-    keysToCamelCase: function(obj) {
-      return convertObjectKeys(obj, 'toCamelCase');
-    }
-  };
-}]);
-
-})(window, window.angular);
-
-(function(window, angular, undefined) {'use strict';
-
-/**
- * Module definition and dependencies
- */
-angular.module('Convert.QueryStringConverter.Service', [
-  'Convert.StringConverter.Service'
-])
-
-/**
- * Factory definition
- */
-.factory('$queryStringConverter', ['$stringConverter', function($stringConverter) {
-
-  /**
-   * Tries to decode an URI component without throwing an exception
-   */
-  function tryDecodeURIComponent(str) {
-    try {
-      return decodeURIComponent(str);
-    } catch (e) {}
-  }
-
-  /**
-   * Tries to encode an URI component without throwing an exception
-   */
-  function tryEncodeURIComponent(str, pctEncodeSpaces) {
-    try {
-      return encodeURIComponent(str)
-        .replace(/%40/g, '@')
-        .replace(/%3A/gi, ':')
-        .replace(/%24/g, '$')
-        .replace(/%2C/gi, ',')
-        .replace(/%20/g, (pctEncodeSpaces ? '%20' : '+'));
-    } catch (e) {}
-  }
-
-  /**
-   * Converts a string to a specific case
-   */
-  function convertToCase(str, convertCase) {
-    switch (convertCase) {
-      case 'snake':
-        return $stringConverter.toSnakeCase(str);
-      case 'camel':
-        return $stringConverter.toCamelCase(str);
-    }
-    return str;
-  }
-
-  /**
-   * Query string converter
-   */
-  return {
-
-    /**
-     * Parses an escaped url query string into key-value pairs
-     */
-    toObject: function(s, convertCase) {
-      var obj = {};
-      var kv, key, val;
-      angular.forEach((s || '').split('&'), function(s) {
-        if (!s) {
-          return;
-        }
-
-        //Split key/value and decode key
-        kv = s.replace(/\+/g, '%20').split('=');
-        key = tryDecodeURIComponent(kv[0]);
-
-        //If not defined, exit
-        if (!angular.isDefined(key)) {
-          return;
-        }
-
-        //Convert case and get value
-        key = convertToCase(key, convertCase);
-        val = angular.isDefined(kv[1]) ? tryDecodeURIComponent(kv[1]) : true;
-
-        //Set property
-        if (!hasOwnProperty.call(obj, key)) {
-          obj[key] = val;
-        }
-        else if (angular.isArray(obj[key])) {
-          obj[key].push(val);
-        }
-        else {
-          obj[key] = [obj[key], val];
-        }
-      });
-      return obj;
-    },
-
-    /**
-     * Convert key-value pairs object to a parametrized query string
-     */
-    fromObject: function(obj, convertCase) {
-
-      //No obj?
-      if (!obj || !angular.isObject(obj)) {
-        return '';
+      //Must have object
+      if (!angular.isObject(obj)) {
+        throw new TypeError('Not an object');
       }
 
-      //Initialize parts array
-      var parts = [];
+      //Validate case
+      if (typeof $stringConverter[converter] === 'undefined') {
+        throw new Error('Invalid converter: ' + converter);
+      }
 
-      //Loop the parameters
-      angular.forEach(obj, function(value, key) {
+      //Initialize object
+      var newObj = {};
 
-        //Skip null/undefined values
-        if (value === null || angular.isUndefined(value)) {
-          return;
+      //Loop keys
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          var newKey = $stringConverter[converter](key);
+          newObj[newKey] = angular.copy(obj[key]);
         }
+      }
 
-        //Convert to array
-        if (!angular.isArray(value)) {
-          value = [value];
-        }
+      //Return object
+      return newObj;
+    }
 
-        //Loop values
-        angular.forEach(value, function(v) {
+    /**
+     * Object converter
+     */
+    return {
 
-          //Handle objects
-          if (angular.isObject(v)) {
-            if (angular.isDate(v)) {
-              v = v.toISOString();
-            }
-            else {
-              v = angular.toJson(v);
-            }
+      /**
+       * Convert object keys to snake case
+       */
+      keysToSnakeCase: function keysToSnakeCase(obj) {
+        return convertObjectKeys(obj, 'toSnakeCase');
+      },
+
+      /**
+       * Convert object keys to snake case
+       */
+      keysToCamelCase: function keysToCamelCase(obj) {
+        return convertObjectKeys(obj, 'toCamelCase');
+      }
+    };
+  }]);
+})(window, window.angular);
+'use strict';
+
+(function (window, angular, undefined) {
+  'use strict';
+
+  /**
+   * Module definition and dependencies
+   */
+
+  angular.module('Convert.QueryStringConverter.Service', ['Convert.StringConverter.Service'])
+
+  /**
+   * Factory definition
+   */
+  .factory('$queryStringConverter', ['$stringConverter', function ($stringConverter) {
+
+    /**
+     * Tries to decode an URI component without throwing an exception
+     */
+    function tryDecodeURIComponent(str) {
+      try {
+        return decodeURIComponent(str);
+      } catch (e) {}
+    }
+
+    /**
+     * Tries to encode an URI component without throwing an exception
+     */
+    function tryEncodeURIComponent(str, pctEncodeSpaces) {
+      try {
+        return encodeURIComponent(str).replace(/%40/g, '@').replace(/%3A/gi, ':').replace(/%24/g, '$').replace(/%2C/gi, ',').replace(/%20/g, pctEncodeSpaces ? '%20' : '+');
+      } catch (e) {}
+    }
+
+    /**
+     * Converts a string to a specific case
+     */
+    function convertToCase(str, convertCase) {
+      switch (convertCase) {
+        case 'snake':
+          return $stringConverter.toSnakeCase(str);
+        case 'camel':
+          return $stringConverter.toCamelCase(str);
+      }
+      return str;
+    }
+
+    /**
+     * Query string converter
+     */
+    return {
+
+      /**
+       * Parses an escaped url query string into key-value pairs
+       */
+      toObject: function toObject(s, convertCase) {
+        var obj = {};
+        var kv, key, val;
+        angular.forEach((s || '').split('&'), function (s) {
+          if (!s) {
+            return;
           }
 
-          //Convert case?
-          switch (convertCase) {
-            case 'snake':
-              key = $stringConverter.toSnakeCase(key);
-              break;
-            case 'camel':
-              key = $stringConverter.toCamelCase(key);
-              break;
+          //Split key/value and decode key
+          kv = s.replace(/\+/g, '%20').split('=');
+          key = tryDecodeURIComponent(kv[0]);
+
+          //If not defined, exit
+          if (!angular.isDefined(key)) {
+            return;
           }
 
-          //Push to parts
-          parts.push(tryEncodeURIComponent(key) + '=' + tryEncodeURIComponent(v));
+          //Convert case and get value
+          key = convertToCase(key, convertCase);
+          val = angular.isDefined(kv[1]) ? tryDecodeURIComponent(kv[1]) : true;
+
+          //Set property
+          if (!hasOwnProperty.call(obj, key)) {
+            obj[key] = val;
+          } else if (angular.isArray(obj[key])) {
+            obj[key].push(val);
+          } else {
+            obj[key] = [obj[key], val];
+          }
         });
-      });
+        return obj;
+      },
 
-      //Any parts?
-      if (parts.length > 0) {
-        return parts.join('&');
+      /**
+       * Convert key-value pairs object to a parametrized query string
+       */
+      fromObject: function fromObject(obj, convertCase) {
+
+        //No obj?
+        if (!obj || !angular.isObject(obj)) {
+          return '';
+        }
+
+        //Initialize parts array
+        var parts = [];
+
+        //Loop the parameters
+        angular.forEach(obj, function (value, key) {
+
+          //Skip null/undefined values
+          if (value === null || angular.isUndefined(value)) {
+            return;
+          }
+
+          //Convert to array
+          if (!angular.isArray(value)) {
+            value = [value];
+          }
+
+          //Loop values
+          angular.forEach(value, function (v) {
+
+            //Handle objects
+            if (angular.isObject(v)) {
+              if (angular.isDate(v)) {
+                v = v.toISOString();
+              } else {
+                v = angular.toJson(v);
+              }
+            }
+
+            //Convert case?
+            switch (convertCase) {
+              case 'snake':
+                key = $stringConverter.toSnakeCase(key);
+                break;
+              case 'camel':
+                key = $stringConverter.toCamelCase(key);
+                break;
+            }
+
+            //Push to parts
+            parts.push(tryEncodeURIComponent(key) + '=' + tryEncodeURIComponent(v));
+          });
+        });
+
+        //Any parts?
+        if (parts.length > 0) {
+          return parts.join('&');
+        }
+
+        //No parts
+        return '';
       }
-
-      //No parts
-      return '';
-    }
-  };
-}]);
-
+    };
+  }]);
 })(window, window.angular);
+'use strict';
 
-(function(window, angular, undefined) {'use strict';
+(function (window, angular, undefined) {
+  'use strict';
 
-/**
- * Module definition and dependencies
- */
-angular.module('Convert.StringConverter.Service', [])
+  /**
+   * Module definition and dependencies
+   */
 
-/**
- * Factory definition
- */
-.factory('$stringConverter', ['$window', function($window) {
-  return {
+  angular.module('Convert.StringConverter.Service', [])
 
-    /**
-     * Convert string to snake case
-     */
-    toSnakeCase: function(str) {
-      if (typeof str === 'number') {
-        return String(str);
-      }
-      else if (typeof str !== 'string') {
-        return '';
-      }
-      if ((str = String(str).trim()) === '') {
-        return '';
-      }
-      return str.replace(/(\s*\-*\b\w|[A-Z])/g, function($1) {
-        $1 = $1.trim().toLowerCase().replace('-', '');
-        return ($1[0] === '_' ? '' : '_') + $1;
-      }).slice(1);
-    },
+  /**
+   * Factory definition
+   */
+  .factory('$stringConverter', ['$window', function ($window) {
+    return {
 
-    /**
-     * Convert string to camel case
-     */
-    toCamelCase: function(str, ucfirst) {
-      if (typeof str === 'number') {
-        return String(str);
-      }
-      else if (typeof str !== 'string') {
-        return '';
-      }
-      if ((str = String(str).trim()) === '') {
-        return '';
-      }
-      return str
-        .replace(/_+|\-+/g, ' ')
-        .replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
+      /**
+       * Convert string to snake case
+       */
+      toSnakeCase: function toSnakeCase(str) {
+        if (typeof str === 'number') {
+          return String(str);
+        } else if (typeof str !== 'string') {
+          return '';
+        }
+        if ((str = String(str).trim()) === '') {
+          return '';
+        }
+        return str.replace(/(\s*\-*\b\w|[A-Z])/g, function ($1) {
+          $1 = $1.trim().toLowerCase().replace('-', '');
+          return ($1[0] === '_' ? '' : '_') + $1;
+        }).slice(1);
+      },
+
+      /**
+       * Convert string to camel case
+       */
+      toCamelCase: function toCamelCase(str, ucfirst) {
+        if (typeof str === 'number') {
+          return String(str);
+        } else if (typeof str !== 'string') {
+          return '';
+        }
+        if ((str = String(str).trim()) === '') {
+          return '';
+        }
+        return str.replace(/_+|\-+/g, ' ').replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
           if (+match === 0) {
             return '';
           }
-          return (index === 0 && !ucfirst) ? match.toLowerCase() : match.toUpperCase();
+          return index === 0 && !ucfirst ? match.toLowerCase() : match.toUpperCase();
         });
-    },
+      },
 
-    /**
-     * Dasherize a string
-     */
-    toDasherized: function(str) {
-      if (typeof str === 'number') {
-        return String(str);
-      }
-      else if (typeof str !== 'string') {
-        return '';
-      }
-      if ((str = String(str).trim()) === '') {
-        return '';
-      }
-      return str.replace(/(\s*\-*\b\w|[A-Z]|_[a-z])/g, function($1) {
-        $1 = $1.replace('_', '-').trim().toLowerCase();
-        return ($1[0] === '-' ? '' : '-') + $1;
-      }).slice(1);
-    },
+      /**
+       * Dasherize a string
+       */
+      toDasherized: function toDasherized(str) {
+        if (typeof str === 'number') {
+          return String(str);
+        } else if (typeof str !== 'string') {
+          return '';
+        }
+        if ((str = String(str).trim()) === '') {
+          return '';
+        }
+        return str.replace(/(\s*\-*\b\w|[A-Z]|_[a-z])/g, function ($1) {
+          $1 = $1.replace('_', '-').trim().toLowerCase();
+          return ($1[0] === '-' ? '' : '-') + $1;
+        }).slice(1);
+      },
 
-    /**
-     * Make the first letter of a string uppercase
-     */
-    toUcFirst: function(str) {
-      if (typeof str !== 'string' && typeof str !== 'number') {
-        return '';
-      }
-      str = String(str).trim();
-      if (str === '') {
-        return '';
-      }
-      return str[0].toUpperCase() + str.substr(1);
-    },
+      /**
+       * Make the first letter of a string uppercase
+       */
+      toUcFirst: function toUcFirst(str) {
+        if (typeof str !== 'string' && typeof str !== 'number') {
+          return '';
+        }
+        str = String(str).trim();
+        if (str === '') {
+          return '';
+        }
+        return str[0].toUpperCase() + str.substr(1);
+      },
 
-    /**
-     * Base 64 decode URL string
-     */
-    fromBase64: function(str) {
-      var o = str.replace('-', '+').replace('_', '/');
-      switch (o.length % 4) {
-        case 0:
-          break;
-        case 2:
-          o += '==';
-          break;
-        case 3:
-          o += '=';
-          break;
-        default:
-          throw 'Illegal base64 url string';
+      /**
+       * Base 64 decode URL string
+       */
+      fromBase64: function fromBase64(str) {
+        var o = str.replace('-', '+').replace('_', '/');
+        switch (o.length % 4) {
+          case 0:
+            break;
+          case 2:
+            o += '==';
+            break;
+          case 3:
+            o += '=';
+            break;
+          default:
+            throw 'Illegal base64 url string';
+        }
+        try {
+          return decodeURIComponent($window.atob(o));
+        } catch (e) {}
       }
-      try {
-        return decodeURIComponent($window.atob(o));
-      }
-      catch (e) {}
-    }
-  };
-}]);
-
+    };
+  }]);
 })(window, window.angular);
